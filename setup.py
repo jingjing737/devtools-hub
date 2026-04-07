236d2ab81d04eee1e1b78d95e51a455c5159862b
jingjing737 <1701432433@qq.com>
2026-04-07 16:37:22 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools-hub",
+    version="2.5.0",
+    packages=find_packages(),
+    install_requires=["flask", "psutil", "requests"],
+    entry_points={
+        "console_scripts": [
+            "devtools=devtools_hub.cli:main"
+        ]
+    },
+    author="jingjing737",
+    description="开发者工具集 - 系统监控、进程管理",
+    url="https://github.com/jingjing737/devtools-hub"
+)
